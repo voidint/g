eb0bc9f74cf1556e89f9bb9c89eb78b96a806c6f
voidint <voidint@126.com>
2019-04-30 23:41:55 +0800

    Add feature: 'g ls'输出已安装go版本列表。#7

--- cli/ls.go
+++ cli/ls.go
@@ -1,9 +1,30 @@
 package cli
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
 	"github.com/urfave/cli"
 )
 
-func list(c *cli.Context) {
+func list(c *cli.Context) (err error) {
+	homeDir, _ := os.UserHomeDir()
 
+	infos, err := ioutil.ReadDir(filepath.Join(homeDir, ".g", "versions"))
+	if err != nil {
+		fmt.Printf("No version installed yet\n\n")
+		return nil
+	}
+	// TODO 排序
+
+	for i := range infos {
+		if !infos[i].IsDir() {
+			continue
+		}
+		fmt.Println(infos[i].Name())
+	}
+	fmt.Println()
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/voidint/g
 go 1.12
 
 require (
+	github.com/Masterminds/semver v1.4.2 // indirect
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
 	github.com/urfave/cli v1.20.0
 )
