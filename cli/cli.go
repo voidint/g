ff8450a710b5fddcac36a056ac97d5696dd0375b
voidint <voidint@126.com>
2019-05-05 13:28:22 +0800

    Modify feature: 修订帮助信息。

--- cli/cli.go
+++ cli/cli.go
@@ -33,25 +33,30 @@ func Run() {
 func init() {
 	cli.AppHelpTemplate = fmt.Sprintf(`NAME:
 	{{.Name}}{{if .Usage}} - {{.Usage}}{{end}}
-
-USAGE:
-	{{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}{{if .Version}}{{if not .HideVersion}}
-
-VERSION:
+ 
+ USAGE:
+	{{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}} {{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}{{if .Version}}{{if not .HideVersion}}
+ 
+ VERSION:
 	%s{{end}}{{end}}{{if .Description}}
-
-DESCRIPTION:
+ 
+ DESCRIPTION:
 	{{.Description}}{{end}}{{if len .Authors}}
-
-AUTHOR{{with $length := len .Authors}}{{if ne 1 $length}}S{{end}}{{end}}:
+ 
+ AUTHOR{{with $length := len .Authors}}{{if ne 1 $length}}S{{end}}{{end}}:
 	{{range $index, $author := .Authors}}{{if $index}}
-	{{end}}{{$author}}{{end}}{{end}}{{if .VisibleFlags}}
-
-OPTIONS:
+	{{end}}{{$author}}{{end}}{{end}}{{if .VisibleCommands}}
+ 
+ COMMANDS:{{range .VisibleCategories}}{{if .Name}}
+ 
+	{{.Name}}:{{end}}{{range .VisibleCommands}}
+	  {{join .Names ", "}}{{"\t"}}{{.Usage}}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+ 
+ GLOBAL OPTIONS:
 	{{range $index, $option := .VisibleFlags}}{{if $index}}
 	{{end}}{{$option}}{{end}}{{end}}{{if .Copyright}}
-
-COPYRIGHT:
+ 
+ COPYRIGHT:
 	{{.Copyright}}{{end}}
 `, build.ShortVersion)
 }
