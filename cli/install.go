9a37c1a802809763c561f1a47f9fbaaa00208986
voidint <voidint@126.com>
2019-05-08 22:15:46 +0800

    Fixbug: '~/.g/downloads'目录不存在情况下无法安装目标版本。#12

--- cli/install.go
+++ cli/install.go
@@ -1,10 +1,7 @@
 package cli
 
 import (
-	"archive/tar"
-	"compress/gzip"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -21,10 +18,15 @@ func install(ctx *cli.Context) (err error) {
 	}
 	homeDir, _ := os.UserHomeDir()
 	rootDir := filepath.Join(homeDir, ".g")
-	versionDir := filepath.Join(rootDir, "versions", vname)
+	downloadsDir := filepath.Join(rootDir, "downloads")
+	versionsDir := filepath.Join(rootDir, "versions")
+	targetV := filepath.Join(versionsDir, vname)
+
+	_ = os.MkdirAll(downloadsDir, 0755)
+	_ = os.MkdirAll(versionsDir, 0755)
 
 	// 检查版本是否已经安装
-	if finfo, err := os.Stat(versionDir); err == nil && finfo.IsDir() {
+	if finfo, err := os.Stat(targetV); err == nil && finfo.IsDir() {
 		return cli.NewExitError(fmt.Sprintf("[g] %q version has been installed.", vname), 1)
 	}
 
@@ -52,7 +54,7 @@ func install(ctx *cli.Context) (err error) {
 		return cli.NewExitError(fmt.Sprintf("[g] %s", err.Error()), 1)
 	}
 
-	filename := filepath.Join(homeDir, ".g", "downloads", fmt.Sprintf("go%s.%s-%s.tar.gz", vname, runtime.GOOS, runtime.GOARCH))
+	filename := filepath.Join(downloadsDir, fmt.Sprintf("go%s.%s-%s.tar.gz", vname, runtime.GOOS, runtime.GOARCH))
 
 	if _, err = os.Stat(filename); os.IsNotExist(err) {
 		// 本地不存在安装包，从远程下载并检查校验和。
@@ -69,57 +71,20 @@ func install(ctx *cli.Context) (err error) {
 		}
 	}
 	// 解压安装包
-	if err = archiver.Unarchive(filename, filepath.Join(rootDir, "versions")); err != nil {
+	if err = archiver.Unarchive(filename, versionsDir); err != nil {
 		return cli.NewExitError(fmt.Sprintf("[g] %s", err.Error()), 1)
 	}
 	// 目录重命名
-	if err = os.Rename(filepath.Join(rootDir, "versions", "go"), versionDir); err != nil {
+	if err = os.Rename(filepath.Join(versionsDir, "go"), targetV); err != nil {
 		return cli.NewExitError(fmt.Sprintf("[g] %s", err.Error()), 1)
 	}
 	// 重新建立软链接
 	goDir := filepath.Join(rootDir, "go")
 	_ = os.Remove(goDir)
 
-	if err := os.Symlink(versionDir, goDir); err != nil {
+	if err := os.Symlink(targetV, goDir); err != nil {
 		return cli.NewExitError(fmt.Sprintf("[g] %s", err.Error()), 1)
 	}
 	fmt.Println("installed successfully")
-	return nil
-}
-
-// unarchive 解压tar.gz文件
-func unarchive(targzFile, dstDir string) (err error) {
-	if err = os.MkdirAll(dstDir, 0755); err != nil {
-		return err
-	}
-
-	srcFile, err := os.Open(targzFile)
-	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-	gr, err := gzip.NewReader(srcFile)
-	if err != nil {
-		return err
-	}
-	defer gr.Close()
-	tr := tar.NewReader(gr)
-	for {
-		hdr, err := tr.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		filename := filepath.Join(dstDir, hdr.Name)
-		os.MkdirAll(filepath.Dir(filename), 0755)
-		f, err := os.Create(filename)
-		if err != nil {
-			return err
-		}
-		io.Copy(f, tr)
-		_ = f.Close()
-	}
 	return nil
 }
