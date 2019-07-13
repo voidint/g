0f5cf7750679a3e3b424ac1531d05e36b73b0138
voidint <voidint@126.com>
2019-07-13 21:55:34 +0800

    Add UT

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -31,12 +32,26 @@ func Test_inuse(t *testing.T) {
 func Test_render(t *testing.T) {
 	Convey("渲染go版本列表", t, func() {
 		var buf strings.Builder
+		v0, _ := semver.NewVersion("1.13-beta1")
 		v1, _ := semver.NewVersion("1.11.11")
 		v2, _ := semver.NewVersion("1.7.0")
 		v3, _ := semver.NewVersion("1.8.1")
-		items := []*semver.Version{v1, v2, v3}
+		items := []*semver.Version{v0, v1, v2, v3}
 
 		render("1.8.1", items, &buf)
-		So(buf.String(), ShouldEqual, "  1.7\n* 1.8.1\n  1.11.11\n")
+		So(buf.String(), ShouldEqual, "  1.7\n* 1.8.1\n  1.11.11\n  1.13beta1\n")
+	})
+}
+
+func Test_wrapstring(t *testing.T) {
+	Convey("包装字符串", t, func() {
+		So(wrapstring("hello world"), ShouldEqual, "[g] Hello world")
+	})
+}
+
+func Test_errstring(t *testing.T) {
+	Convey("返回错误字符串", t, func() {
+		So(errstring(nil), ShouldBeBlank)
+		So(errstring(errors.New("hello world")), ShouldEqual, "[g] Hello world")
 	})
 }
