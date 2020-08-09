fa786327c5bbee5bfcc1ad81fb0b92dab1563922
voidint <voidint@126.com>
2020-08-09 21:16:39 +0800

    Upgrade version: 1.1.3 -> 1.2.0

--- build/build.go
+++ build/build.go
@@ -4,7 +4,7 @@ import "strings"
 
 const (
 	// ShortVersion 短版本号
-	ShortVersion = "1.1.3"
+	ShortVersion = "1.2.0"
 )
 
 // The value of variables come form `gb build -ldflags '-X "build.Build=xxxxx" -X "build.CommitID=xxxx"' `
