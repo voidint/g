a8bf3f50127f4fd642a8fb05fdc9e75f20006cdc
voidint <voidint@126.com>
2022-11-23 18:56:27 +0800

    refactor: optimize the installation script. #47

--- install.sh
+++ install.sh
@@ -33,40 +33,52 @@ main() {
     local release="1.4.0"
     local os=$(get_os)
     local arch=$(get_arch)
-    local dest_file="${HOME}/g${release}.${os}-${arch}.tar.gz"
+    local dest_file="${HOME}/.g/downloads/g${release}.${os}-${arch}.tar.gz"
     local url="https://github.com/voidint/g/releases/download/v${release}/g${release}.${os}-${arch}.tar.gz"
 
     echo "[1/3] Downloading ${url}"
     rm -f "${dest_file}"
     if [ -x "$(command -v wget)" ]; then
-        wget -q -P "${HOME}" "${url}"
+        wget -q -P "${HOME}/.g/downloads" "${url}"
     else
-        curl -s -S -L -o "${dest_file}" "${url}"
+        curl -s -S -L --create-dirs -o "${dest_file}" "${url}"
     fi
 
-    echo "[2/3] Install g to the ${HOME}/bin"
-    mkdir -p "${HOME}/bin"
-    tar -xz -f "${dest_file}" -C "${HOME}/bin"
-    chmod +x "${HOME}/bin/g"
+    echo "[2/3] Install g to the ${HOME}/.g/bin"
+    mkdir -p "${HOME}/.g/bin"
+    tar -xz -f "${dest_file}" -C "${HOME}/.g/bin"
+    chmod +x "${HOME}/.g/bin/g"
 
     echo "[3/3] Set environment variables"
+    cat >${HOME}/.g/env <<-'EOF'
+#!/bin/sh
+# g shell setup
+export GOROOT="${HOME}/.g/go"
+export PATH="${HOME}/.g/bin:${HOME}/.g/go/bin:$PATH"
+export G_MIRROR=https://golang.google.cn/dl/
+	EOF
+
+
     if [ -x "$(command -v bash)" ]; then
         cat >>${HOME}/.bashrc <<-'EOF'
-		# ===== set g environment variables =====
-		export GOROOT="${HOME}/.g/go"
-		export PATH="${HOME}/bin:${HOME}/.g/go/bin:$PATH"
-		export G_MIRROR=https://golang.google.cn/dl/
+# g shell setup
+if [ -f "${HOME}/.g/env" ]; then
+    . "${HOME}/.g/env"
+fi
 		EOF
     fi
 
     if [ -x "$(command -v zsh)" ]; then
         cat >>${HOME}/.zshrc <<-'EOF'
-		# ===== set g environment variables =====
-		export GOROOT="${HOME}/.g/go"
-		export PATH="${HOME}/bin:${HOME}/.g/go/bin:$PATH"
-		export G_MIRROR=https://golang.google.cn/dl/
+# g shell setup
+if [ -f "${HOME}/.g/env" ]; then
+    . "${HOME}/.g/env"
+fi
 		EOF
     fi
+
+
+    echo -e "\nTo configure your current shell, run:\nsource \"$HOME/.g/env\""
 
     exit 0
 }
