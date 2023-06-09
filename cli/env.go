674d15f65b6704df837cc22239df5224c0e6707f
akayj <askingyj@gmail.com>
2023-06-09 10:48:27 +0800

    show unset env

--- cli/env.go
+++ cli/env.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
 )
 
 func showEnv(ctx *cli.Context) (err error) {
 	var envVars = []string{experimentalEnv, homeEnv, mirrorEnv}
 
-	for _, v := range envVars {
-		fmt.Printf("%s = %s\n", v, os.Getenv(v))
+	for _, e := range envVars {
+		v, set := os.LookupEnv(e)
+		if !set {
+			color.New(color.FgYellow).Fprintf(os.Stdout, "%s=(UNSET)\n", e)
+		} else {
+			fmt.Printf("%s=%s\n", e, v)
+		}
 	}
 	return nil
 }
