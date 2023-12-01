f90ce967e82a017b284f3db847aacaf4e2625a62
voidint <voidint@126.com>
2023-12-01 12:14:08 +0800

    misc: upgrade version number

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 param (
-    [string] $release = "1.5.2",
+    [string] $release = "1.6.0",
     [string] $base_dir
 )
 
--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ function get_os() {
 }
 
 main() {
-    local release="1.5.2"
+    local release="1.6.0"
     local os=$(get_os)
     local arch=$(get_arch)
     local dest_file="${HOME}/.g/downloads/g${release}.${os}-${arch}.tar.gz"
